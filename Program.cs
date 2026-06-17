311fbe5c4502355dadb7de63d8b67d427b6cbe3c
wutthiphat sakulpong <wutthiphat.sa.67@ubu.ac.th>
2026-06-17 10:20:24 +0700

    Learning Arrays

--- Program.cs
+++ Program.cs
@@ -18,6 +18,14 @@ namespace LearnCS
              * klklkl
              * klklklkl
             */
+            string[] cars = new string[3];
+            cars[0] = "Volvo";
+            cars[1] = "BMW";
+            cars[2] = "Ford";
+            Console.WriteLine($"First car : {cars[0]}");
+            Console.WriteLine($"Second car : {cars[1]}");
+            Console.WriteLine($"Third car : {cars[2]}");
+            // Arrays
         }
     }
 }
